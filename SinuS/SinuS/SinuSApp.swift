a364821c73d6f034e7577faa8692d8b7cb35b21d
Loe Hendriks <loehendriks@live.nl>
2022-08-28 15:33:29 +0200

    initial project commit

---
+++ SinuS/SinuS/SinuSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinuSApp.swift
+//  SinuS
+//
+//  Created by Loe Hendriks on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SinuSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
