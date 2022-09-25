5bfa9783ce090b005541e696203674e965d71b27
Loe Hendriks <loehendriks@live.nl>
2022-09-25 11:16:17 +0200

    Fix bug where graph could not be opened after creating new user

--- SinuS/SinuS/Views/NewUserView.swift
+++ SinuS/SinuS/Views/NewUserView.swift
@@ -45,6 +45,9 @@ struct NewUserView: View {
                 
                 Button("Add User!") {
                     self.manager.AddUser(user: self.username, target: self.targetname)
+                    
+                    // Add initial point at zero
+                    self.manager.SendData(data: SinusUpdate(name: self.username, password: "", value: 0, date: Date()))
                 }.padding()
             }
             .foregroundColor(.white)
