076478149d13bdaf578bb308446ea1a6f5a010bd
Patrick van Broeckhuijsen <github@patb.ro>
2023-01-08 14:08:13 +0100

    Fix GraphDataPoint struct and add LogHelper to DataGatherer

---
+++ SinuS/SinuS/Data/Log/LogHelper.swift
@@ -0,0 +1,35 @@
+//
+//  LogHelper.swift
+//  SinuS
+//
+//  Created by Patrick van Broeckhuijsen on 08/01/2023.
+//
+
+import Foundation
+
+public class LogHelper {
+    private static var logUrl = "https://lukassinus2.vanbroeckhuijsenvof.nl/api/log"
+
+    public func logMsg(level: String = "debug", message: String) {
+        let parameters: [String: Any] = ["level": level, "message": message]
+        let decoder = JSONDecoder()
+        var request = RestApiHelper.createRequest(type: "PUT", url: LogHelper.logUrl, auth: true)
+
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+            return
+        }
+
+        let data = RestApiHelper.perfomRestCall(request: request)
+
+        do {
+            _ = try decoder.decode(AuthenticationResult.self, from: data!)
+        } catch {
+            print("Unexpected error: \(error).")
+        }
+
+        return
+    }
+}
